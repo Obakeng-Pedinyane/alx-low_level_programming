c5513b9ba42ab0090d876493dc175eceffa40b23
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-07 11:31:02 -0700

    C program the prints 'Programming is like building a multilingual puzzle'

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints a string using puts function.
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+
+{
+	puts("Programming us like building a multilingual puzzle \n");
+
+
+
+
+	return (0);
+
+
+}
