0fd33226cb969db2b1af44c20af20d90b4362338
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-11 01:06:02 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * main - Function prints alphabet in reverse
+ *
+ * Return: always 0 (Success)
+ *
+ **/
+
+int main(void)
+{
+	char letter;
+
+	for ( letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+	
+	return (0);
+}
