3e8cfe2096a2be3e8f34e8cd715c2910e08b38d9
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-11 00:21:20 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Function prints sigle digit numbers
+ *
+ * Return: always 0 (Success)
+ *
+ **/
+
+int main(void)
+{
+	int x;
+
+	while (x < 10)
+	{
+		pr("%d", x)
+			x = x + 1
+
+	}
+	return (0)
+}
