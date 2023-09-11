e36c41d393a396f1fe53962e008a904b2b7b1b00
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-10 21:13:44 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,9 @@
 
 int main(void)
 {
-	char letter = 'a';
+	char letter;
 
-	for (letter; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		if (letter != 'e' && letter != 'q')
 			putchar(letter);
 
