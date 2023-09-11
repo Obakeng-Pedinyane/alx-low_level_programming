1c489975d854e67310f197135c570e4f97983b0e
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-10 21:08:35 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Function prints all letters except q,e
+ *
+ * Return: always 0 (Success)
+ **/
+
+int main(void)
+{
+	char letter = 'a';
+
+	for (letter; letter <= 'z'; letter++)
+		if (letter != 'e' && letter != 'q')
+			putchar(letter)
+
+	putchar('\n');
+	return (0);
+}
