e856cc2917489af19f03c7bcef5f98ca2f0a68d7
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-12 11:37:43 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "1-main.c"
+
+/**
+ * print_alphabet - Function prints alphabet
+ *
+ * Return: always void (Success)
+ *
+ **/
+
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar('x');
+
+	}
+	_putchar('\n');
+}
