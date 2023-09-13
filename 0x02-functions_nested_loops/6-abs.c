4a8a0936c8126b2a01e0247d63c70e664c006769
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-13 05:18:28 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Function prints abs value of n
+ *
+ * @n: Argument of type int being passed
+ *
+ * Return: always 0 (Success)
+ **/
+
+int _abs(int n)
+{
+	int result;
+
+	result = abs(n);
+	_putchar(result);
+
+	return (0);
+}
