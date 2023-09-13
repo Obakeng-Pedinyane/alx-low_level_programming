d93da51cc66e994b472dd240bf84131814afc6de
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-13 13:18:42 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - Function adds two numbers
+ *
+ * @a: Function argument
+ *
+ * @b: Function argument
+ * Return: always sum of two arguments
+ *
+ */
+
+int add(int a, int b)
+{
+	int ans;
+
+	ans = a + b;
+
+	return (ans);
+}
