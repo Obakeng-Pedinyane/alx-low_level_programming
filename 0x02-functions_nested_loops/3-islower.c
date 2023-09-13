9c14cd0afdc1e6b21d1d3fa962d7ab1b8ec02138
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-13 02:20:34 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -13,12 +13,11 @@ int _islower(int c)
 {
 	if (islower(c) == 0)
 	{
-		_putchar('0');
+		return ('0');
 	}
-	if (islower(c) == 1)
+	else
 	{
-		_putchar('1');
+		return ('1');
 	}
 	_putchar('\n');
-	return (0);
 }
