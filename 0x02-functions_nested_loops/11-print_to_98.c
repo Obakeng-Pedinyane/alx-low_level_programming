716427dba4be0e9e51b415a55e8a58a8f7517c6b
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-13 13:52:49 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Function prints to 98
+ *
+ * @n: Funtion argument
+ *
+ * Return: always void (Success)
+ */
+
+void print_to_98(int n)
+{
+	int s;
+
+	for (s = n; s < 99; s++)
+	{
+		if (s == 98)
+		{
+			printf("%d\n", s);
+		}
+		else
+		{
+			printf("%d, ", s);
+		}
+	}
+}
