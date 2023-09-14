eaad3cf77b501c7c1e5ec3be0ca5ec3398b865dc
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-14 08:05:02 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - Function prints more numbers
+ *
+ * Return: always void (Success)
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		int j;
+
+		for (j = 0; j <= 14; j++)
+		{
+			if (j <= 9)
+			{
+				_putchar('0' + j);
+			}
+			else
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
