bde9c53ca742435c7b0834c4197769d40692a1d7
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-14 08:19:58 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,15 +16,12 @@ void more_numbers(void)
 
 		for (j = 0; j <= 14; j++)
 		{
-			if (j <= 9)
-			{
-				_putchar('0' + j);
-			}
-			else
+			if (j > 9)
 			{
 				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
 			}
+
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
