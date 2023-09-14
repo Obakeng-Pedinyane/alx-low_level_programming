cad2f44863519daf61dc73ea5d0d09da0bb8709e
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-14 06:26:34 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function prints all single digits
+ *
+ * Return: always void (Success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
