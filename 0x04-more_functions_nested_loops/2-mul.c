820e1b38c65ef7e49569db6f7cc21955d1eb7121
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-14 05:45:19 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - Function prints product of two arguments
+ *
+ * @a: Function argument
+ *
+ * @b: Function argument
+ *
+ * Return: always value of 'res'
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = (a * b);
+	return (res);
+
+}
