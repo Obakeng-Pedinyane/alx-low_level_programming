adeba157c773e5fb491e9f7618f6ec2b6093c63f
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-14 06:40:39 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Function prints most numbers
+ *
+ * Return: always void (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+		}
+	}
+}
