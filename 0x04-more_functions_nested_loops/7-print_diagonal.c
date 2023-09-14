29354d1deb98da07e19b4ebb230e5b2d9bf61506
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-14 15:21:04 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_diagonal - function prints straight line
+ *
+ * @n: function argument
+ *
+ * Return: always void Success.
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int a;
+
+		for (a = 0; a <= n; a++)
+		{
+			int b;
+
+			for (b = 0; b < a; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
