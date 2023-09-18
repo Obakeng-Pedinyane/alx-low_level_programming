d6504fdb45315505c81227dbe0c494d2008ed264
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-18 07:46:10 -0700

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+#include "_putchar.c"
+
+/**
+ * rev_string - function reverses a string
+ *
+ * @s: function parameter
+ *
+ * Return: always void.
+ */
+void rev_string(char *s)
+{
+	int le;
+	int count;
+
+	le = strlen(s) - 1;
+	for (count = 0; count <= le; count++)
+	{
+		_putchar(s[count]);
+	}
+	_putchar('\n');
+	for (count = le; count >= 0; count--)
+	{
+		_putchar(s[count]);
+	}
+	_putchar('\n');
+}
