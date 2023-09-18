1f7f6366f3d9d3a3153722e30fff24593311d3ab
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-18 06:03:12 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function prints string to stdout
+ *
+ * @str: function parameter
+ *
+ * Return: always void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
