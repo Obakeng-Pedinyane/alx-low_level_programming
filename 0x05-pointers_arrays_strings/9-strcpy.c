fef44bf7853ad81ef40f8ec6143f8a52fb63567d
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-18 19:42:59 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/*
+ * _strcpy - function copies one string from one pointer to another
+ *
+ * @dest: funtion parameter destination pointer
+ *
+ * @src: function parameter source pointer
+ *
+ * Return: always *dest {Success)
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
