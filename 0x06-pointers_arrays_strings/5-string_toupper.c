c886e36dbd1d2426b7bd99ae4f156c19a30fadca
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-20 11:23:52 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,9 +9,15 @@
  *
  * Return: uppercase version of function argument
  */
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
 	int l;
+	int i;
 
-	l = strlen(
+	l = strlen(s);
+	for (i = 0; i < l; i++)
+	{
+		s[i] = toupper(s[i]);
+	}
+	return (s);
 }
