27513f7dabc52e46942bb62d49c37fdb2f1739e9
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-09-20 12:17:33 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * cap_string - function capitalizes all words of a string
+ *
+ * @s: function parameter
+ *
+ * Return: always new s string.
+ */
+char *cap_string(char *s)
+{
+	int cn;
+	int i;
+
+	cn = 1;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (isalpha(s[i]) || isdigit(s[i]))
+		{
+			if (cn)
+			{
+				s[i] = toupper(s[i]);
+				cn = 0;
+			}
+			else
+			{
+				s[i] = tolower(s[i]);
+			}
+		}
+		else
+		{
+			cn = 1;
+		}
+
+	}
+	return (s);
+}
