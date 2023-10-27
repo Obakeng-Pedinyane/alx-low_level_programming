749afeac5d8bf58f3565d49cc03c60962ec867d5
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-10-27 14:00:57 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+/**
+ * main - function adds positive numbers
+ *
+ * @argv: parameters
+ *
+ * @argc: parameters
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	count = 0;
+	if (argc > 1)
+	{
+		int i;
+
+		for (i = 1; i < argc; i++)
+		{
+			if (isdigit(*argv[i]))
+			{
+				count += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", count);
+	}
+	else if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	return (0);
+}
