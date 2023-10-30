841f1bd2444fa625b35235ad0a9dae7644d099be
Obakeng-Pedinyane <obakengpedinyane@gmail.com>
2023-10-30 05:44:24 -0700

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - function creates an array of chars, and inits it to c
+ *
+ * @size: size of array
+ *
+ * @c: character initialized
+ *
+ * Return: Alwayss an array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *answer;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		answer = malloc(sizeof(c) * size);
+		for (i = 0;i < size;i++)
+		{
+			answer[i] = c;
+		}
+		return (answer);
+	}
+	free(answer);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef Header_File
+#define Header_File
+
+char *create_array(unsigned int size, char c);
+
+#endif
